e0f6c3a2ebffc99384d4a59817868eb2f8af7605
Eric Oliver <eric.t.oliver@dime.dev>
2023-07-19 16:08:08 -0500

    adding the initial function from the demo

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,17 @@
+package com.pluralsight.courseinfo.cli;
+
+public class CourseRetriever {
+	public static void main(String... args){
+		System.out.println("CourseRetriever started!");
+		if (args.length == 0){
+			System.out.println("Please provide an auther name as first argument.");
+			return;
+		}
+
+		retrieveCourses(args[0]);
+	}
+
+	private static void retrieveCourses(String authorId) {
+		System.out.println("Retrieving courses for author " + authorId);
+	}
+}
